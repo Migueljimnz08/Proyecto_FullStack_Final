f2648600d77d30ec6bee815b3ac24157d699b70b
Miguel <migueljm1008@gmail.com>
2025-09-13 22:22:58 +0200

    add/express-validator middleware and controllers for users, favs and recipes

---
+++ controllers/favs.controller.js
@@ -0,0 +1,40 @@
+const favModel = require('../models/favs.model');
+
+// GET http://localhost:3000/api/favorites/id
+const getFavsByUser = async (req, res) => {
+  const { userID } = req.params;
+  try {
+    const favs = await favModel.getFavsByUser(userID);
+    res.status(200).json(favs);
+  } catch (error) {
+    res.status(500).json({ error: 'Error getting favs' });
+  }
+};
+
+// POST http://localhost:3000/api/favorites
+const addFav = async (req, res) => {
+  const { userID, recetaID } = req.body;
+  try {
+    const fav = await favModel.addFav(userID, recetaID);
+    res.status(201).json({ msg: 'Recepy added to favs', data: fav });
+  } catch (error) {
+    res.status(500).json({ error: 'Error adding fav' });
+  }
+};
+
+// DELETE http://localhost:3000/api/favorites
+const deleteFav = async (req, res) => {
+  const { userID, recetaID } = req.body;
+  try {
+    await favModel.deleteFav(userID, recetaID);
+    res.status(200).json({ msg: 'Recepy deleted from favs' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error deleting fav' });
+  }
+};
+
+module.exports = {
+    getFavsByUser,
+    addFav,
+    deleteFav
+}
